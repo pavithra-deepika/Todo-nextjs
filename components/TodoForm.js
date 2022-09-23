727a8f6b1fc69ac23f4c690cb44ced1ce0d6c937
pavithra <pavithradeepika301@gmail.com>
2022-09-23 12:54:25 +0530

    Add New Todolist

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,11 +1,30 @@
+import { async } from "@firebase/util"
 import{Button, TextField} from "@mui/material"
+import { addDoc, serverTimestamp } from "firebase/firestore"
+import { useState } from "react"
+import {db} from "../firebase"
+import { collection, doc, setDoc, orderBy, onSnapshot,query} from "@firebase/firestore"; 
+
+
 const TodoForm = () => {
+    const [todo, setTodo] =useState({ title:"", detail:""})
+    const onSubmit = async () => {
+        const collectionRef = collection(db, "todos")
+        const docRef =await addDoc(collectionRef,{...todo, timestamp:serverTimestamp() })
+        setTodo({ title:'',detail:''})
+        alert (`Todo with id ${docRef.id} is added sucessfully`)
+    }
     return (
         <div>
-            <TextField fulWidth label="title" margin="normal"/>
-            <TextField fullWidth label="detail" multiline maxRows={4}/>
-            <Button sx={ {mt:3}}>Add anew todo </Button>
-
+            <TextField fulWidth label="title" margin="normal"
+            value={todo.title}
+            onChange={e => setTodo({ ...todo, title: e.target.value})}
+            />
+            <TextField fullWidth label="detail" multiline maxRows={4}
+             value={todo.detail}
+             onChange={e => setTodo({ ...todo, detail: e.target.value})}
+             />
+            <Button onClick={onSubmit}  variant="contained" sx={ {mt:3}}>Add anew todo </Button>
 
         </div>
 
